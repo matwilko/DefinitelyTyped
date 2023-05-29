a8ab5dcda1357edce1f7c9ab924f4f17689d9741
Matt Wilkinson <mawilkinson@microsoft.com>
2023-05-29 21:06:02 +0100

    WIP

--- types/deep-freeze/deep-freeze-tests.ts
+++ types/deep-freeze/deep-freeze-tests.ts
@@ -1,5 +1,13 @@
 import df = require('deep-freeze');
 
+type IsEqual<A, B> =
+	(<G>() => G extends A ? 1 : 2) extends
+	(<G>() => G extends B ? 1 : 2)
+		? true
+		: false;
+
+type Test<T, Expected> = IsEqual<df.DeepReadonly<T>, Expected>;
+
 class Foo {
     foo: string;
 }
@@ -8,3 +16,34 @@ const items = df([{id: 0, name: 'first'}]);
 const functionTest = df({id: 0, name: 'first', update: (blah: boolean) => blah});
 
 functionTest.update(true);
+
+type MapsToSelf<T> = Test<T, T>;
+const unknownMapsToUnknown: MapsToSelf<unknown> = true;
+const anyMapsToAny: MapsToSelf<any> = true;
+const neverMapsToNever: MapsToSelf<never> = true;
+const nullMapsToNull: MapsToSelf<null> = true;
+const undefinedMapsToUndefined: MapsToSelf<undefined> = true;
+const stringMapsToString: MapsToSelf<string> = true;
+const numberMapsToNumber: MapsToSelf<number> = true;
+const booleanMapsToBoolean: MapsToSelf<boolean> = true;
+const symbolMapsToSymbol: MapsToSelf<symbol> = true;
+const bigintMapsToBigint: MapsToSelf<bigint> = true;
+const functionMapsToFunction: MapsToSelf<Function> = true;
+const typedFunctionMapsToSelf: MapsToSelf<(a: number) => string> = true;
+
+const emptyArray: Test<[], readonly []> = true;
+const primitiveArray: Test<number[], readonly number[]> = true;
+const objectArray: Test<{ a: string }[], readonly { readonly a: string }[]> = true;
+const arrayOfPrimitiveArrays: Test<(number[])[], readonly (readonly number[])[]> = true;
+const arrayOfObjectArrays: Test<({ a: string }[])[], readonly (readonly { readonly a: string }[])[]> = true;
+
+const tupleElements: Test<
+[number, string, { a: string }, number[], { a: string }[], { a: string[] }],
+readonly [number, string, { readonly a: string }, readonly number[], readonly { readonly a: string }[], { readonly a: readonly string[] }]
+> = true;
+
+const emptyObject: Test<{}, {}> = true;
+const simpleObject: Test<{ a: number }, { readonly a: number }> = true;
+const nestedObject: Test<{ a: { b: number } }, { readonly a: { readonly b: number } }> = true;
+const arrayNestedInObject: Test<{ a: number[] }, { readonly a: readonly number[] }> = true;
+const objectArrayNestedInObject: Test<{ a: { b: number }[] }, { readonly a: readonly { readonly b: number }[] }> = true;
--- types/deep-freeze/index.d.ts
+++ types/deep-freeze/index.d.ts
@@ -6,13 +6,113 @@
 
 export = deepFreeze;
 
-declare function deepFreeze<T>(a: T[]): ReadonlyArray<deepFreeze.DeepReadonly<T>>;
-declare function deepFreeze<T extends Function>(f: T): T;
 declare function deepFreeze<T>(o: T): deepFreeze.DeepReadonly<T>;
 
+type IsEqual<A, B> =
+	(<G>() => G extends A ? 1 : 2) extends
+	(<G>() => G extends B ? 1 : 2)
+		? true
+		: false;
+
 declare namespace deepFreeze {
-    type DeepReadonly<T> =
-        T extends (...args: any) => any
+    type TypesNotChanged = never | null | undefined | string | number | boolean | symbol | bigint | Function;
+
+    type DeepReadonly<T> = IsEqual<T, unknown> extends true
+        ? unknown
+        : T extends TypesNotChanged
             ? T
             : { readonly [P in keyof T]: DeepReadonly<T[P]> };
+}
+
+
+// TODO:
+// Type definitions for deep-freeze 0.1
+// Project: https://github.com/substack/deep-freeze
+// Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Aluan Haddad <https://github.com/aluanhaddad>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.1
+
+export = deepFreeze;
+
+type Primitive = null | undefined | string | number | boolean | symbol | bigint;
+type IfTrue<T, TTrue = true, TFalse = false> = T extends true ? TTrue : TFalse;
+type IfNever<T, TTrue = true, TFalse = false> = T extends never ? TTrue : TFalse;
+type IfAny<T0, T1, T2 = false, T3 = false, T4 = false, T5 = false, T6 = false, T7 = false, T8 = false, T9 = false, T10 = false> = IfTrue<T0, true, IfTrue<T1, true, IfTrue<T2, true, IfTrue<T3, true, IfTrue<T4, true, IfTrue<T5, true, IfTrue<T6, true, IfTrue<T7, true, IfTrue<T8, true, IfTrue<T9, true, IfTrue<T10>>>>>>>>>>>;
+
+type HasNeverPropertiesDeep<T> = ({
+	[Key in keyof T]: CollapseNever<T[Key]> extends never ? true : false
+}[keyof T]) extends false ? false : true;
+
+type HasNeverTupleElements<T> = T extends [infer T0, ...(infer TRest)]
+	? IfNever<T0, true, IfTrue<HasNeverTupleElements<TRest>>>
+	: T extends readonly [infer T0, ...(infer TRest)]
+		? IfNever<T0, true, IfTrue<HasNeverTupleElements<TRest>>>
+		: false;
+
+type HasNeverArrayElements<T> = T extends []
+	? false
+	: T extends readonly []
+		? false
+		: T extends never[]
+			? true
+			: T extends readonly never[]
+				? true
+				: false;
+
+type IsTupleType<T> = IfAny<T extends [infer T0, ...(infer TRest)] ? true : false, T extends readonly [infer T0, ...(infer TRest)] ? true : false>;
+
+type IsArrayType<T> = IfAny<T extends [] ? true : false, T extends readonly[] ? true : false, T extends any[] ? true : false, T extends readonly any[] ? true : false>;
+
+type CollapseNever<T> = T extends Primitive
+	? T
+	: IsTupleType<T> extends true
+		? IfTrue<HasNeverTupleElements<T>, never, T>
+		: IsArrayType<T> extends true
+			? IfTrue<HasNeverArrayElements<T>, never, T>
+			: IfTrue<HasNeverPropertiesDeep<T>, never, T>;
+
+
+declare function deepFreeze<T>(obj: T): deepFreeze.DeepReadonly<T>;
+
+declare namespace deepFreeze {
+    type TypesThatThrow = DataView | Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
+    type TypesUnsupported = Map<any, any> | WeakMap<any, any> | WeakSet<any> | Set<any> | Promise<any>;
+    type TypesNotChanged = never | null | undefined | string | number | boolean | symbol | bigint | Function;
+
+    type DeepReadonly<T> = CollapseNever<
+        T extends TypesThatThrow
+            ? never
+            : T extends TypesNotChanged
+                ? T
+                : { readonly [P in keyof T]: DeepReadonly<T[P]> }
+    >;
+}
+
+type PartFreeze<T extends TNotFrozen, TNotFrozen> = IsEmptyObject<Except<T, keyof TNotFrozen>> extends true
+	? TNotFrozen
+	: TNotFrozen & deepFreeze.DeepReadonly<Except<T, keyof TNotFrozen>>;
+
+interface FrozenMap<Key, Value> extends Readonly<Map<Key, Value>> {}
+interface FrozenWeakMap<Key extends object, Value> extends Readonly<WeakMap<Key, Value>> {}
+interface FrozenSet<Value> extends Readonly<Set<Value>> {}
+interface FrozenWeakSet<Value extends object> extends Readonly<WeakSet<Value>> {}
+interface FrozenArrayBuffer extends Readonly<ArrayBuffer> {}
+
+declare namespace deepFreeze {
+    type TypesUnsupported = ArrayBuffer | DataView | Promise<any>;
+    type TypesNotChanged = never | null | undefined | string | number | boolean | symbol | bigint | Function | TypesUnsupported;
+
+    type DeepReadonly<T> = T extends Map<infer Key, infer Value>
+        	? PartFreeze<T, FrozenMap<Key, Value>>
+			: T extends WeakMap<infer Key, infer Value>
+				? PartFreeze<T, FrozenWeakMap<Key, Value>>
+				: T extends WeakSet<infer Value>
+					? PartFreeze<T, FrozenWeakSet<Value>>
+					: T extends Set<infer Value>
+						? PartFreeze<T, FrozenSet<Value>>
+							: T extends ArrayBuffer
+								? PartFreeze<T, ArrayBuffer>
+								: T extends DataView
+
+							: { readonly [P in keyof T]: DeepReadonly<T[P]> };
 }
